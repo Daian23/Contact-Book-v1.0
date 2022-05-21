4d5a854dbf224912ea8990a379c0f1860ff41fc5
Daiana23 <daiana23jofre@gmail.com>
2022-05-21 01:25:10 -0300

    Routes

---
+++ routes/index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const ClaseContact = require('../ClaseContact');
+const ClaseContactBook = require('../ClaseContactBook');
+const apps = require('../models/prueba');
+
+router.get('/',(request,response) =>{
+    response.render('menu.ejs', {contactsBooks: apps.filesDirectory()});
+});
+
+router.get('/addContact',(request,response) =>{
+
+    response.render('addContact.ejs', {contactsBooks: apps.filesDirectory(), mensaje: "Hola"});
+   
+});
+
+router.post('/addContact',(request,response)=>{
+    
+    let nombre = request.body.name;
+    let email = request.body.email;
+    let mobil = request.body.mobil;
+    let topList = request.body.topList;
+    let contactBook = request.body.ContactB;
+
+    if(!nombre || !email || !mobil || !topList || !contactBook){
+        response.send(400).send("Loa campos no pueden estar vacíos");
+    }
+
+    if(apps.validateMobil(contactBook,mobil)){
+        msj="Este contacto no fue registrado porque ya existe";
+    }else{
+        var tp = apps.convertFavorite(topList);
+        var contact = new ClaseContact.Contact(nombre,email,mobil,tp);
+        apps.saveContact(contactBook,JSON.stringify(contact));
+        msj="Contacto registrado con exito";
+    }
+   
+    response.redirect('/addContact');
+
+});
+
+router.get('/home/:CB',(request,response) =>{
+
+    var contactsBook = apps.loadContacts(`${request.params.CB}`);
+    console.log("Name CB",contactsBook.name);
+    response.render('listContacts.ejs', {contactsBooksL: contactsBook, contactsBooks: apps.filesDirectory()});
+    
+
+});
+
+router.get('/eliminarContacto/:CB/:mobil',(request,response)=>{
+
+   apps.deleteContact(`${request.params.CB}`,request.params.mobil);
+
+   var contactsBook = apps.loadContacts(`${request.params.CB}`);
+
+   //response.render('listContacts.ejs', {contactsBooksL: contactsBook, contactsBooks: apps.filesDirectory()});
+   response.redirect('/home/:CB');
+});
+
+router.get('/addContactBook',(request,response) =>{
+
+    response.render('CreateContactBook.ejs',{contactsBooks: apps.filesDirectory(), mensaje:"hola"});
+
+});
+
+router.post('/addContactBook',(request,response)=>{
+    
+    let nameCB = request.body.name;
+
+    var msj = apps.saveCB(nameCB);
+
+    response.redirect('/addContactBook');
+});
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const ClaseContact = require('./ClaseContact');
-const ClaseContactBook = require('./ClaseContactBook');
-const apps = require('./models/prueba');
+
 
 //const { send } = require('process');
 app.use(express.json());
@@ -21,75 +19,11 @@ app.use(express.static(__dirname + "/public/"));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-app.get('/',(request,response) =>{
-    response.render('menu.ejs', {contactsBooks: apps.filesDirectory()});
-});
-
-app.get('/addContact',(request,response) =>{
-
-    response.render('addContact.ejs', {contactsBooks: apps.filesDirectory(), mensaje: "Hola"});
-   
-});
-
-app.post('/salidaContact',(request,response)=>{
-    
-    let nombre = request.body.name;
-    let email = request.body.email;
-    let mobil = request.body.mobil;
-    let topList = request.body.topList;
-    let contactBook = request.body.ContactB;
-
-    if(apps.validateMobil(contactBook,mobil)){
-        msj="Este contacto no fue registrado porque ya existe";
-    }else{
-        var tp = apps.convertFavorite(topList);
-        var contact = new ClaseContact.Contact(nombre,email,mobil,tp);
-        apps.saveContact(contactBook,JSON.stringify(contact));
-        msj="Contacto registrado con exito";
-    }
-   
-    response.render('addContact.ejs', {contactsBooks: apps.filesDirectory(),mensaje: msj});
-
-});
-
-app.get('/home/:CB',(request,response) =>{
-
-    var contactsBook = apps.loadContacts(`${request.params.CB}`);
-    console.log("Name CB",contactsBook.name);
-    response.render('listContacts.ejs', {contactsBooksL: contactsBook, contactsBooks: apps.filesDirectory()});
-    
-
-});
-
-app.post('/salidaCB',(request,response)=>{
-    
-    let nameCB = request.body.name;
-
-    var msj = apps.saveCB(nameCB);
-
-    response.render('CreateContactBook.ejs', {contactsBooks: apps.filesDirectory(),mensaje: msj});
-
-    //Solucionar
-    //response.sendFile(path.join(__dirname,'./forms/salidaCB.html'));
-    
-});
-
-app.get('/eliminarContacto/:CB/:mobil',(request,response)=>{
-
-   apps.deleteContact(`${request.params.CB}`,request.params.mobil);
-
-   var contactsBook = apps.loadContacts(`${request.params.CB}`);
-
-   response.render('listContacts.ejs', {contactsBooksL: contactsBook, contactsBooks: apps.filesDirectory()});
-});
-
-app.get('/CreateContactBook',(request,response) =>{
-
-    response.render('CreateContactBook.ejs',{contactsBooks: apps.filesDirectory(), mensaje:"hola"});
-
-});
+//routes
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT,()=>{
   console.log(`Express listen on port ${PORT}`);
